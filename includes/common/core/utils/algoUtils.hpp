5ecc43b943dfe57649ad9a8ee552bf7e969a129e
pieric-ux <demont.pieric@gmail.com>
2025-10-15 23:55:21 +0200

    feat: algoUtils with swap

---
+++ includes/common/core/utils/algoUtils.hpp
@@ -0,0 +1,59 @@
+// TODO: Don't forget to add 42 header !
+
+#ifndef ALGOUTILS_HPP
+#define ALGOUTILS_HPP
+
+/**
+ * @file UniquePtr.hpp
+ * @brief
+ */
+
+#include <cstddef>
+
+namespace common
+{
+namespace core
+{
+namespace utils
+{
+
+/**
+ * @brief 
+ *
+ * @tparam T 
+ * @param a 
+ * @param b 
+ */
+template<typename T>
+void	swap(T *a, T *b)
+{
+	T *tmp = a;
+	a = b;
+	b = tmp;
+}
+
+/**
+ * @brief 
+ *
+ * @tparam T 
+ * @param a 
+ * @param N 
+ * @param b 
+ * @param N 
+ */
+template<typename T, std::size_t N>
+void	swap(T (&a)[N], T (&b)[N])
+{
+	for (std::size_t i = 0; i < N; ++i)
+	{
+		T tmp = a[i];
+		a[i] = b[i];
+		b[i] = a[i];
+	}
+}
+
+} // !utils
+} // !core
+} // !common
+
+#endif // !ALGOUTILS_HPP
