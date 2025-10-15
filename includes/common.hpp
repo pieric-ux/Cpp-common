fc57fc49397b5eb88059ca871218123741086c6c
pieric-ux <demont.pieric@gmail.com>
2025-10-16 00:08:27 +0200

    feat: common.hpp

---
+++ includes/common.hpp
@@ -0,0 +1,23 @@
+// TODO: Don't forget to add 42 header !
+
+#ifndef COMMON_HPP
+#define COMMON_HPP
+
+/**
+ * @file common.hpp
+ * @brief
+ */
+
+#include "common/core/raii/Deleters.hpp"
+#include "common/core/raii/SharedPtr.hpp"
+#include "common/core/raii/UniquePtr.hpp"
+
+#include "common/core/utils/algoUtils.hpp"
+#include "common/core/utils/Directory.hpp"
+#include "common/core/utils/fileUtils.hpp"
+#include "common/core/utils/stringUtils.hpp"
+#include "common/core/utils/timeUtils.hpp"
+
+#include "loader/Loader.hpp"
+
+#endif // !COMMON_HPP
